5e11862f989dafdc905465bc52fe21f34f1ecf03
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-10-02 13:55:03 -0400

    add of custom file input

--- public/js/cms.js
+++ public/js/cms.js
@@ -131,4 +131,10 @@ $(document).ready(function() {
         window.location.href = "/blog";
       });
   }
+
+  // Add the following code if you want the name of the file appear on select
+  $(".custom-file-input").on("change", function() {
+    var fileName = $(this).val().split("\\").pop();
+    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+  });
 });
