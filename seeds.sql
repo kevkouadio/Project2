b4688586d606d9e60b33db3d70ce9b2490c59cab
Kouassi Kevin Kouadio <kevkouadio@gmail.com>
2020-10-07 18:25:01 -0400

    Update seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO blogger (recipe_name, new) VALUES ('Chicken Parmesan', FALSE);
-INSERT INTO blogger (recipe_name, new) VALUES ('Filet Mignon with Shrimp', FALSE);
-INSERT INTO blogger (recipe_name, new) VALUES ('Colorado Omlet', FALSE);
+INSERT INTO Authors (Name) VALUES ('Wendy');
+INSERT INTO Authors (Name) VALUES ('Tasheena');
+
+
+INSERT INTO Posts (title, body, AuthorId) VALUES ('Chicken Parmesan', 'breaded chicken breast covered in tomato sauce and mozzarella, parmesan, or provolone cheese.', 1);
+INSERT INTO Posts (title, body, AuthorId) VALUES ('Filet Mignon with Shrimp', 'petite filet mignon topped with shrimp scampi in a garlic butter sauce and served with creamy spinach and artichoke.', 2);
+INSERT INTO Posts (title, body, AuthorId) VALUES ('Colorado Omlet', 'omelette stuffed with bacon, shredded beef, pork sausage & ham with green peppers, onions & Cheddar cheese. Served with our salsa.', 1);
